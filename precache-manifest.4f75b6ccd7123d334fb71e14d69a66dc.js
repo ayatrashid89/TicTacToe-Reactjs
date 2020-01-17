0dc962348312140be5f592d2ecf23138ee0cae78
Ayat Rashid <ayatrashid@students.highline.edu>
2020-01-17 13:48:17 -0800

    Updates

---
+++ precache-manifest.4f75b6ccd7123d334fb71e14d69a66dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6097e30baddca26b968157f4412f4fd0",
+    "url": "/TicTacToe-Reactjs/index.html"
+  },
+  {
+    "revision": "6553e73664636b2e0a6b",
+    "url": "/TicTacToe-Reactjs/static/css/main.0b4e3724.chunk.css"
+  },
+  {
+    "revision": "fe7d20e5e350a3adeb7d",
+    "url": "/TicTacToe-Reactjs/static/js/2.d1bd8194.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TicTacToe-Reactjs/static/js/2.d1bd8194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6553e73664636b2e0a6b",
+    "url": "/TicTacToe-Reactjs/static/js/main.92c1f89d.chunk.js"
+  },
+  {
+    "revision": "829b8b4cca0bfcfb3ab4",
+    "url": "/TicTacToe-Reactjs/static/js/runtime-main.9c537976.js"
+  },
+  {
+    "revision": "58938df4757c64adf952572480c78dde",
+    "url": "/TicTacToe-Reactjs/static/media/bg2tic.58938df4.jpg"
+  },
+  {
+    "revision": "b0734d2575aa054adf6563f435ac1da7",
+    "url": "/TicTacToe-Reactjs/static/media/bg4tic.b0734d25.png"
+  }
+]);
